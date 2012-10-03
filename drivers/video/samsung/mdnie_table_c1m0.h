47ac8e7e690b3bdf973c7d384632d94f67c39168
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-10-03 04:03:22 -0700

    mdniemod: remove hardcore's sharpness tweaks and make a misc device for all mdnie classes

--- drivers/video/samsung/mdnie_table_c1m0.h
+++ drivers/video/samsung/mdnie_table_c1m0.h
@@ -609,7 +609,7 @@ static const unsigned short tune_warm_outdoor[] = {
 	END_SEQ, 0x0000,
 };
 
-struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
+static struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 	{
 		{
 			{"NORMAL",		NULL},
@@ -624,7 +624,7 @@ struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 	}
 };
 
-struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
+static struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	{
 		{
 			{"CYANOGENMOD",		tune_cyanogenmod},
@@ -670,7 +670,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	}
 };
 
-struct mdnie_tunning_info camera_table[OUTDOOR_MAX] = {
+static struct mdnie_tunning_info camera_table[OUTDOOR_MAX] = {
 	{"CAMERA",		tune_camera},
 	{"CAMERA_OUTDOOR",	tune_camera_outdoor},
 };
